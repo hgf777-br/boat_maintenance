38aa5eef76c099d06d8f5520095cacd29755c91a
Henrique Faria <hgf777@gmail.com>
2025-04-16 18:57:38 -0300

    datatables changed for bootstrap table

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 
 from .views import (
@@ -17,10 +17,11 @@ urlpatterns = [
     path('update/<int:pk>', UserUpdateView.as_view(), name='update-user'),
     path('delete/<int:pk>', UserDeleteView.as_view(), name='delete-user'),
     path('send_new_password/<int:pk>', UserSendNewPasswordView.as_view(), name='send-new-password-user'),
-    path('change-password/',UserChangePasswordView.as_view(), name='change-password-user'),
+    path('change-password/', UserChangePasswordView.as_view(), name='change-password-user'),
     path(
         'profile/<int:pk>/',
         TemplateView.as_view(template_name='users/profile.html'),
         name='profile',
     ),
+    path('teste/', TemplateView.as_view(template_name='user/teste.html'), name='teste'),
 ]
