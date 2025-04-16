df0e2e34de636c8d559da4c1277589901796ed27
Henrique Faria <hgf777@gmail.com>
2025-04-16 19:48:03 -0300

    new cron

--- maintenance/cron.py
+++ maintenance/cron.py
@@ -11,7 +11,7 @@ def send_email_for_maintenances():
     message = ''
     for maintenance in pending_maintenances:
         message += f"Manutenção: {maintenance.description}\n"
-        message += f"Data: {maintenance.date}\n"
+        message += f"Data: {maintenance.due_date}\n"
         message += f"Técnico: {maintenance.technician.name}\n\n"
     send_mail(
         "Teste de email de manutenção",
